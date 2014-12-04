206c6b6f2c54fc56df8b7aeb2d220027c86dcfa2
fengyadong <fengyadong@feinno.com>
2014-12-04 11:22:46 +0800

    UPDATE README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ server-sdk-python
 =================
 
 Rong Cloud Server SDK in Python.
+
+python setup.py install
+
+from rongcloud.api import ApiClient
+client = ApiClient("youappkey", "youappsecret")
--- test/test_helper.py
+++ test/test_helper.py
@@ -10,8 +10,8 @@ import unittest
 logging.basicConfig(level=logging.INFO)
 
 # Credential
-app_key = "8brlm7ufr6893"
-app_secret = "LCLvqdWSupCzFH"
+app_key = ""
+app_secret = ""
 
 os.environ.setdefault('rongcloud-app-key', app_key)
 os.environ.setdefault('rongcloud-app-secret', app_secret)
