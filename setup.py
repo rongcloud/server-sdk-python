4bcc68581a89a06b81f8bdc0169b8933d505ba48
zlraymind <zlraymind@outlook.com>
2019-06-11 11:14:49 +0800

    Release 3.0.2.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rc-server-sdk",
+    version="3.0.2",
+    author="zhanglei1",
+    author_email="zhanglei1@rongcloud.cn",
+    description="rongcloud python server sdk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rongcloud/server-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
